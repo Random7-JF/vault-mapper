1dce51a789d356d146b9d06d144ad6054214b60a
Jon Fingas <jon.fingas@gmail.com>
2023-03-19 13:45:16 -0500

    basic get and post function with templates

--- app/main.go
+++ app/main.go
@@ -1,13 +1,41 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html"
+)
 
 func main() {
-	app := fiber.New()
+	// Create a new engine
+	htmlEngine := html.New("./views", ".html")
+	htmlEngine.Debug(true)
+	htmlEngine.Reload(true)
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World")
+	app := fiber.New(fiber.Config{
+		Views: htmlEngine,
 	})
+	routes(app)
 
 	app.Listen(":3000")
+}
+
+func routes(app *fiber.App) {
+
+	app.Get("/", indexHandler)
+	app.Post("/", postIndexHandler)
+}
+
+func indexHandler(c *fiber.Ctx) error {
+	// Render index
+	return c.Render("pages/index", fiber.Map{
+		"Title": "Hello, World!",
+	})
+}
+
+func postIndexHandler(c *fiber.Ctx) error {
+	test := c.FormValue("test")
+	fmt.Println(test)
+	return c.SendString(test)
 }
